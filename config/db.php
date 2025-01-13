f937bdcf0215dafe5c5f309aac3620c5b356a6ad
JIHANE ELMAJDI <emleyamanda9@gmail.com>
2025-01-13 15:44:52 +0100

    Refactorisation de la connexion à la base de données en utilisant la POO dans le fichier db.php.

--- config/db.php
+++ config/db.php
@@ -1,27 +1,35 @@
 <?php
 
 class Database {
-    private $host = "localhost";
-    private $dbname = "youdemy";
-    private $username = "root";
-    private $password = "";
-    private $pdo;
+    private $db;
 
-  
     public function __construct() {
         try {
-            $this->pdo = new PDO("mysql:host={$this->host};dbname={$this->dbname}", $this->username, $this->password);
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Connexion réusssie";
+            $this->db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS);
+            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
-            die("Erreur de connexion : " . $e->getMessage());
+            die('Erreur de connexion : ' . $e->getMessage());
         }
     }
 
-    
-    public function getConnection() {
-        return $this->pdo;
+    private function exec($request, $values = null) {
+        $stmt = $this->db->prepare($request);
+        $stmt->execute($values);
+        return $stmt;
+    }
+
+    public function setFetchMode($fetchMode) {
+        $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, $fetchMode);
+    }
+
+    public function execute($request, $values = array()) {
+        $result = $this->exec($request, $values);
+        return ($result) ? true : false;
+    }
+
+    public function fetch($request, $values = null, $all = true) {
+        $result = $this->exec($request, $values);
+        return ($all) ? $result->fetchAll() : $result->fetch();
     }
 }
-
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
+require_once 'config/config.php';
 require_once 'config/db.php';
 
-$db = new Database();
 
-$pdo = $db->getConnection();
-?>
