52ba2b28eccc35a81addcd1d824da00e9bde0ad7
JIHANE ELMAJDI <emleyamanda9@gmail.com>
2025-01-13 11:45:26 +0100

    Création de la base de données dans phpMyAdmin et ajout du fichier de configuration db.php pour la connexion à la base de données.

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost";    
+$dbname = "youdemy";    
+$username = "root";     
+$password = "";         
+
+try {
+   
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion réussie !";  
+} catch (PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
+}
+?>
