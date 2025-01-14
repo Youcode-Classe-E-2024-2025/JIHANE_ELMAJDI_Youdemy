b7992f5ec767571aa1ddbf1fb93f670694630467
JIHANE ELMAJDI <emleyamanda9@gmail.com>
2025-01-14 11:30:31 +0100

    Restauration du fichier config.php pour la configuration du projet.

---
+++ classes/Database.php
@@ -0,0 +1,34 @@
+// class Database.php
+<?php
+class Database {
+    private $db;
+
+    public function __construct() {
+        try {
+         
+            $this->db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS);
+            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die('Erreur de connexion : ' . $e->getMessage());
+        }
+    }
+
+    public function getConnection() {
+        return $this->db;
+    }
+
+    public function execute($request, $values = array()) {
+      
+        $stmt = $this->db->prepare($request);
+        $stmt->execute($values);
+        return $stmt;
+    }
+
+    public function fetch($request, $values = null) {
+     
+        $stmt = $this->db->prepare($request);
+        $stmt->execute($values);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
+?>
