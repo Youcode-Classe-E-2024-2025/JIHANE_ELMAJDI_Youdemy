998df9e766c86afd99ab981484b5842f47913218
JIHANE ELMAJDI <emleyamanda9@gmail.com>
2025-01-15 13:07:34 +0100

    Correction des erreurs dans le fichier de configuration pour résoudre les problèmes de connexion.

--- config/config.php
+++ config/config.php
@@ -1,6 +1,10 @@
 <?php
+// Database configuration
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'youdemy');
-define('DB_USER', 'root'); 
-define('DB_PASS', '');  
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+// Other configurations
+define('SITE_URL', 'http://localhost/JIHANE_ELMAJDI_Youdemy');
 ?>
